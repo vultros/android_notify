9aeb4416db30ff2dd012a11e116964d720f7040f
vultros <luckow@ohane.de>
2013-03-02 21:29:33 +0100

    adding .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AndroidNotify::Application.config.secret_token = 'a0db70b87263ec5aa81fd95135aebaa98f9153297af9b6585009260a88f9b3eab92b78a9b4c2104eeebadaa443e35d4a0fa55414866013bf9a6a50b8ac1aee3b'
